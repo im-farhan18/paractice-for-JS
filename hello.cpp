c1430f820e3a37380846779900d60153ac94541a
Farhan Khan <khanfarhan2358@gmail.com>
2026-07-29 20:27:29 +0530

    just checking the commits

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,6 @@ int main(){
     cout << "assalamualikum World!" << endl;
     cout << "walaikum assalam!" << endl;
     cout << "how are you?" << endl;
+    cout << "I am fine, thank you!" << endl;
     return 0;
 }
