d653fa6cbf7158558f99cb6502aca7fa889f62a5
Farhan Khan <khanfarhan2358@gmail.com>
2026-07-30 17:49:17 +0530

    today i have to cover the events topic

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+button =document.createElement('button');
+button.textContent = 'Click Me';
+button.style.backgroundColor = 'blue';
+button.style.color = 'white';
 
-console.log("JavaScript Connected!");
+document.querySelector("body").prepend(button)
+
+let p = document.querySelector("p")
+p.classList.add("newclass")
