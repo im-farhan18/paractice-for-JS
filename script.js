5b225c3ca589be074940ef6c2c384e45816cd198
Farhan Khan <khanfarhan2358@gmail.com>
2026-08-01 23:46:26 +0530

    leraned about how the event objects

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let div = document.querySelector("div");
-div.onclick = () =>{
-    console.log("div was clicked")
+
+let button = document.createElement("button")
+button.style.backgroundColor = " rgb(159, 80, 80)";
+button.style.color ="black";
+button.innerText= "click me";
+document.querySelector("body").prepend(button)
+
+button.onclick = (evt) => {
+    console.log(evt.type);
+    alert("its done bro !");
+    let a =18;
+    a++
+    console.log(a);
 }
