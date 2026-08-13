c32cc469b22d8964270add6af985ff58675c3c97
Farhan Khan <khanfarhan2358@gmail.com>
2026-08-13 13:18:52 +0530

    hii for a day

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,4 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout << "assalamualikum World!" << endl;
-    cout << "walaikum assalam!" << endl;
-    cout << "how are you?" << endl;
-    cout << "I am fine, thank you!" << endl;
-    return 0;
-}
+int main()
--- script.js
+++ script.js
@@ -1,14 +1,43 @@
 
-let button = document.createElement("button")
+let button = document.createElement("button");
+button.style.backgroundColor = " rgb(159, 80, 80)";
+button.style.color ="black";
+button.innerText= "click me";
+document.querySelector("body").prepend(button)
+
+
+
+
+let button = document.createElement("button");
 button.style.backgroundColor = " rgb(159, 80, 80)";
 button.style.color ="black";
 button.innerText= "click me";
 document.querySelector("body").prepend(button)
 
 button.onclick = (evt) => {
-    console.log(evt.type);
-    alert("its done bro !");
-    let a =18;
-    a++
-    console.log(a);
+    let div = document.createElement("div");
+    div.style.height ="100px";
+    div.style.width ="100px";
+    div.innerText ="a event div";
+    div.style.backgroundColor ="blue";
+    div.style.color ="black";
+    div.style.border ="solid 4px pink"
+    
+    document.querySelector("body").prepend(div)
+
+}
+
+
+
+button.onclick = (evt) => {
+    let div = document.createElement("div");
+    div.style.height ="100px";
+    div.style.width ="100px";
+    div.innerText ="a event div";
+    div.style.backgroundColor ="blue";
+    div.style.color ="black";
+    div.style.border ="solid 4px pink"
+    
+    document.querySelector("body").prepend(div)
+
 }
