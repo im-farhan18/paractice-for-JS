545e2873ee63b285b6504df5238cdd3b0edabc77
Farhan Khan <khanfarhan2358@gmail.com>
2026-08-23 13:41:48 +0530

    Remove user input and sum calculation from hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -2,9 +2,6 @@
 using namespace std;
 
 int main() {
-    int a, b;
-    cout << "Enter two numbers: ";
-    cin >> a >> b;
-    cout << "Sum = " << a + b << endl;
+    cout << "0";
     return 0;
 }
