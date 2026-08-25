f0c75959a9d171f2516f62a96424ef79d40f3831
Farhan Khan <khanfarhan2358@gmail.com>
2026-08-26 00:31:16 +0530

    Update hello.cpp to calculate and display PI with higher precision

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#define PI 3.14
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    cout << "*******\n******\n*****\n****\n***\n**\n*";
+    float PI1 = 3.141234567891011121314151617;
+    double PI2 = 3.141234567891011121314151617;
+    cout<<"PI1 ="<<setprecision(20)<<PI1<<endl<<"PI2 ="<<setprecision(20)<<PI2<<endl;
     return 0;
 }
