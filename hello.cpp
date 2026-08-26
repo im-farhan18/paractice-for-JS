a33afe618c8eee3464cb060a990333d75b7c195c
Farhan Khan <khanfarhan2358@gmail.com>
2026-08-26 19:46:03 +0530

    Refactor hello.cpp to remove PI calculations and restore original output pattern

--- hello.cpp
+++ hello.cpp
@@ -4,8 +4,9 @@ using namespace std;
 
 int main()
 {
-    float PI1 = 3.141234567891011121314151617;
-    double PI2 = 3.141234567891011121314151617;
-    cout<<"PI1 ="<<setprecision(20)<<PI1<<endl<<"PI2 ="<<setprecision(20)<<PI2<<endl;
+    int n = 5;
+
+    cout<<"****\n***\n**\n*";
+
     return 0;
 };
