23aee07715319be734b90d3504965f593b8bab01
Farhan Khan <khanfarhan2358@gmail.com>
2026-08-27 10:21:40 +0530

    Refactor hello.cpp to calculate total bill with GST based on user input for pen, pencil, and eraser amounts

--- hello.cpp
+++ hello.cpp
@@ -4,16 +4,24 @@ using namespace std;
 
 int main()
 {
-    int a;
-    int b;
-    cin >> a;
-    cout << "Enter the 1st value";
-    cin >> b;
-    cout << "Enter the 2nd value";
+    float pen;
+    float pencil;
+    float eraser;
 
-    int sum = a + b;
+    cout << "Enter the Amount of pen :";
+    cin >> pen;
 
-    cout<<"Sum = "<<sum<<endl;
+    cout << "Enter the Amount of pencil :";
+    cin >> pencil;
+
+    cout << "Enter the Amount of Eraser :";
+    cin >> eraser;
+
+    float amount = pen + pencil + eraser;
+
+    float toal_amount = amount + (0.18 * amount);
+
+    cout << "your tatal BILL with GST is $" << toal_amount  << endl;
 
     return 0;
 };
