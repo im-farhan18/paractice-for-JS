7a077a0c2b04aff30a3355e47774e43510e36082
Farhan Khan <khanfarhan2358@gmail.com>
2026-08-29 19:04:27 +0530

    Update hello.cpp to initialize variable 'a' and display values of 'a', 'b1', and 'b2'

--- hello.cpp
+++ hello.cpp
@@ -3,10 +3,15 @@ using namespace std;
 
 int main()
 {
-    int a = 3;
-    a++;
+    int a = 10;
+    int b1 = a++;
+    int b2 = ++a;
 
-    cout<<"a = "<<a<<endl;
+    cout<<"a ="<<a<<endl;
+        cout<<"b1 ="<<b1<<endl;
+            cout<<"b2 ="<<b2<<endl;
+
+
 
     return 0;
 };
