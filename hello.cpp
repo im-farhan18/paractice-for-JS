8075b69c6b938517344a4538a48d6b706a6949e5
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-01 18:20:39 +0530

    Refactor hello.cpp to remove unused variables and input logic for three numbers

--- hello.cpp
+++ hello.cpp
@@ -3,27 +3,7 @@ using namespace std;
 
 int main()
 {
-    int a, b, c;
-
-    cout << "Enter 3  numbers :" << endl;
-
-    cout << "Enter a :";
-    cin >> a;
-
-    cout << "Enter b :";
-    cin >> b;
-
-    cout << "Enter c :";
-    cin >> c;
-
-    if (a > b)
-    {
-        cout << a << " is the larger then" << b << endl;
-    }
-    else
-    {
-        cout << b << "is the larger then" << a << endl;
-    }
+    int a,b;
 
     return 0;
 };
