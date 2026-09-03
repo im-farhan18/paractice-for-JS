7a9b0ff22cc5af0383af55aa100cdb5024e9ec67
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-03 10:49:02 +0530

    Refactor hello.cpp to modify the loop for printing asterisks instead of a greeting message

--- hello.cpp
+++ hello.cpp
@@ -3,11 +3,15 @@ using namespace std;
 
 int main()
 {
-    int n = 5;
+    int n = 3;
 
     for (int row = 1; row <= n; row++)
     {
-        cout << "Assalamualaikum world! " << endl;
+        for(int col = 1;col<= n;col++)
+        {
+            cout << "*";
+        }
+        cout << endl;
     }
 
     return 0;
