84b6724faf5c0cb07525f7b09836a12edd057401
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-03 15:51:50 +0530

    Refactor hello.cpp to update the initial value of n and remove digit summation logic

--- hello.cpp
+++ hello.cpp
@@ -2,20 +2,11 @@
 using namespace std;
 
 int main()
-{
-    int n = 10898;
-    int digsum = 0;
 
-    while (n > 0)
-    {
-        int lastdigit = n % 10;
-        if( lastdigit % 2 != 0)
-        {
-            digsum += lastdigit;
-        }
-        cout << lastdigit << " ";
-        n = n / 10;
-    }
-    cout << "Sum of digits: " << digsum << endl;
+{
+
+    int n = 10829;
+
+    
     return 0;
 };
