04161d9fe28811fc1c6d70bdd17359081cd2d6c2
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-05 13:08:19 +0530

    Refactor hello.cpp to rename variable and improve digit extraction logic

--- hello.cpp
+++ hello.cpp
@@ -5,8 +5,13 @@ int main()
 
 {
 
-    int n = 10829;
+    int num = 10829;
+    while (num > 0)
+    {
+        int digit = num % 10;
+        cout << digit << " ";
+        num /= 10;
+    }
 
-    
     return 0;
 };
