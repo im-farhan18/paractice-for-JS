0614a4849354b376fffb0f73df15405cffd9c7db
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-06 00:12:19 +0530

    Refactor hello.cpp to update number input logic and modify factorial calculation

--- hello.cpp
+++ hello.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 
 {
 
-    int num = 10829;
-    while (num > 0)
-    {
-        int digit = num % 10;
-        cout << digit << " ";
-        num /= 10;
+    int num;
+    cout << "Enter the number :" << endl;
+    cin >> num;
+
+    for (int i = 2; i >= num -1 ; i++)
+
+    if ( num % i == 0 ) {
+         int fact =  fact* i;
+        cout << i << " is a factorial of NUMber :"<< endl;
     }
+    else {
+        cout <<"*"<<endl;
+    }
+
 
     return 0;
 };
