1c33e56d9a6de45cff5b3c527e42d41829b1312c
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-06 23:39:19 +0530

    Refactor hello.cpp to modify loop logic for output formatting

--- hello.cpp
+++ hello.cpp
@@ -11,12 +11,25 @@ int main()
 
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = 1; j <= n; j++)
         {
-            cout << " *";
+            cout << i<< " ";
         }
-        cout<<endl;
+        cout << endl; 
     }
 
     return 0;
 };
+
+/*  
+output :
+
+Enter a number :8
+1 1 1 1 1 1 1 1 
+2 2 2 2 2 2 2 2 
+3 3 3 3 3 3 3 3 
+4 4 4 4 4 4 4 4 
+5 5 5 5 5 5 5 5 
+6 6 6 6 6 6 6 6 
+7 7 7 7 7 7 7 7 
+8 8 8 8 8 8 8 8 /*
