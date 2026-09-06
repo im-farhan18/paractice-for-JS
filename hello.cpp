ac0429ae7183560cfecce31b6c70ebc9337985fc
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-06 23:59:22 +0530

    Refactor hello.cpp to update loop logic for pyramid pattern output

--- hello.cpp
+++ hello.cpp
@@ -11,9 +11,9 @@ int main()
 
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = 1; j <= (n-i); j++)
         {
-            cout << " *";
+            cout << "* ";
         }
         cout << endl; 
     }
@@ -24,13 +24,14 @@ int main()
 /*  
 output :
 
-Enter a number :6
+Enter a number :8
 
- *
- * *
- * * *
- * * * *
- * * * * *
- * * * * * *
+* * * * * * * 
+* * * * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
 
  */
