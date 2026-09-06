3508cfe47df3e46f67e1e227a0017fddb866bac9
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-06 12:24:06 +0530

    Refactor hello.cpp to update input prompt and remove unused prime number logic

--- hello.cpp
+++ hello.cpp
@@ -7,13 +7,9 @@ int main()
 {
 
     int n;
-    cout << "Enter the number :" << endl;
-    cin >> n;
-    bool isPrime = true;
-
-    if (int i = 2; i<= n-1; i++){
-        if()
-    }
+    cout<< "Enter a number :"<<endl;
+    cin>>n;
+    
 
         return 0;
 };
