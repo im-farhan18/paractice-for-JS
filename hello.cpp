a85cc81c0ff078b86242b18ec033e503c730ff50
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-07 17:57:05 +0530

    Refactor hello.cpp to update loop logic for number pattern output

--- hello.cpp
+++ hello.cpp
@@ -11,27 +11,29 @@ int main()
 
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= (n-i); j++)
+        for (int j = 1; j <= i; j++)
         {
-            cout << "* ";
+            cout << j<<"";
         }
-        cout << endl; 
+        cout << endl;
     }
 
     return 0;
 };
 
-/*  
+/*
 output :
 
 Enter a number :8
 
-* * * * * * * 
-* * * * * * 
-* * * * * 
-* * * * 
-* * * 
-* * 
-* 
+1
+12
+123
+1234
+12345
+123456
+1234567
+12345678
+
 
  */
