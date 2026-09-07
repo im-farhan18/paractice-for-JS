0259b22a5d6d672e67b6097dbc4ab4a5558c26d2
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-07 18:21:40 +0530

    Refactor hello.cpp to change output from numbers to letters in pattern generation

--- hello.cpp
+++ hello.cpp
@@ -8,14 +8,14 @@ int main()
     int n;
     cout << "Enter a number :";
     cin >> n;
+    char ch= 'A';
 
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= i; j++)
-        {
-            cout << j<<"";
+        for (int j = 1; j <= i; j++){
+            cout<<ch++;
         }
-        cout << endl;
+        cout<<endl;
     }
 
     return 0;
@@ -24,16 +24,10 @@ int main()
 /*
 output :
 
-Enter a number :8
-
-1
-12
-123
-1234
-12345
-123456
-1234567
-12345678
-
+Enter a number :4
+A
+BC
+DEF
+GHIJ
 
  */
