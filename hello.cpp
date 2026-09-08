6e0f16e83a2b9bb8b82154792070621f84e722aa
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-08 23:17:32 +0530

    Refactor hello.cpp to simplify input handling and adjust pyramid pattern output

--- hello.cpp
+++ hello.cpp
@@ -5,43 +5,21 @@ using namespace std;
 int main()
 
 {
-    int n;
-    cout << "Enter a number :";
-    cin >> n;
+    int n = 8;
 
     for (int i = 1; i <= n; i++)
     {
-        cout << "*";
-
-        for (int j = 1; j <= n - 1; j++)
+        for (int j = 1; j <= n - i; j++)
         {
-            if (i == 1 || i == n)
-            {
-                cout << " * ";
-            }
-            else
-            {
-                cout << "   ";
-            }
+            cout << " ";
         }
 
-        cout << "*" << endl;
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
     }
 
     return 0;
 };
-
-/*
-output :
-
-Enter a number :8
-* *  *  *  *  *  *  * *
-*                     *
-*                     *
-*                     *
-*                     *
-*                     *
-*                     *
-* *  *  *  *  *  *  * *
-
- */
