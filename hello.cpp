149213568827230701583b3ac5fa505f639bfd97
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-09 12:39:20 +0530

    Refactor hello.cpp to reorganize loops for pyramid pattern generation and improve readability

--- hello.cpp
+++ hello.cpp
@@ -7,39 +7,46 @@ int main()
 {
     int n = 5;
 
-    for (int i = n; i >= 1; i--)
-    {
-        // Spaces
-        for (int j = 1; j <= n - i; j++)
-        {
-            cout << " ";
-        }
-        // Stars
-        for (int j = 1; j <= 2 * i - 1; j++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
-
-
-    // OUTER LOOP
+    // Upper half
     for (int i = 1; i <= n; i++)
     {
-        // Spaces
-        for (int j = 1; j <= n - i; j++)
+
+        for (int j = 1; j <= i; j++)
         {
-            cout << " ";
+            cout<<"*";
         }
-        // Stars
-        for (int j = 1; j <= 2 * i - 1; j++)
+
+        for ( int j =1;j<=2*(n-i);j++){
+           cout<<" "; 
+        }
+
+        for (int j = 1; j <= i; j++)
         {
-            cout << "*";
+            cout<<"*";
         }
-        cout << endl;
+        cout<<endl;
     }
 
-    
+
+
+    for (int i = n; i>= 1; i--)
+    {
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<"*";
+        }
+
+        for ( int j =1;j<=2*(n-i);j++){
+           cout<<" "; 
+        }
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
 
     return 0;
 };
