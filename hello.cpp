21229577b98c66ac97d604ac92f6106b006de3b3
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-09 12:11:43 +0530

    Refactor hello.cpp to adjust pyramid height from 10 to 5 and modify loop order

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,23 @@ using namespace std;
 int main()
 
 {
-    int n = 10;
+    int n = 5;
+
+    for (int i = n; i >= 1; i--)
+    {
+        // Spaces
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        // Stars
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
 
     // OUTER LOOP
     for (int i = 1; i <= n; i++)
@@ -23,20 +39,7 @@ int main()
         cout << endl;
     }
 
-    for (int i = n; i >= 1; i--)
-    {
-        // Spaces
-        for (int j = 1; j <= n - i; j++)
-        {
-            cout << " ";
-        }
-        // Stars
-        for (int j = 1; j <= 2 * i - 1; j++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
+    
 
     return 0;
 };
