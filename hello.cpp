8f0342f172e01e1cb639e30d57b8851718fe2f41
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-13 01:30:53 +0530

    Refactor hello.cpp to change binTOdec function parameter type back to int

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 
-void binTOdec(long int binNum)
+void binTOdec(int binNum)
 {
       int n = binNum;
       int dec = 0;
@@ -19,9 +19,27 @@ void binTOdec(long int binNum)
       cout << dec << endl;
 }
 
+void decTobin(int decnum)
+{
+      int n = decnum;
+      long long  pow = 1;
+      long long  binNum = 0;
+
+      while (n > 0)
+      {
+            int rem = n % 2;
+            binNum += rem * pow;
+            n = n / 2;
+            pow = pow * 10;
+      }
+
+      cout << binNum << endl;
+}
+
 int main()
 {
 
-      binTOdec(11630110);
+      binTOdec(100011100);
+      decTobin(20000084);
       return 0;
 }
