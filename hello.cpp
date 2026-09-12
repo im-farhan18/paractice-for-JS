ebd88cb5ea91f266a97533f56337b883917fe87d
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-12 12:22:21 +0530

    Refactor hello.cpp for consistent formatting in nextchar function

--- hello.cpp
+++ hello.cpp
@@ -2,15 +2,15 @@
 #include <cmath>
 using namespace std;
 
-char nextchar(char c ){
-      cout<<"the next alphabet after "<<c<<" is : "<<endl;
+char nextchar(char c)
+{
+      cout << "the next alphabet after " << c << " is : " << endl;
       return c + 1;
 }
 
 int main()
 {
 
-      
-      cout<<nextchar('D');
+      cout << nextchar('D');
       return 0;
 }
