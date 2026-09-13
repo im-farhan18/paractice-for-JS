24d28682ff4f2480031fbddee3634a7856cae55e
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-13 16:35:02 +0530

    Refactor hello.cpp to replace binTOdec function with findsum for summing digits

--- hello.cpp
+++ hello.cpp
@@ -2,44 +2,24 @@
 #include <cmath>
 using namespace std;
 
-void binTOdec(int binNum)
+int findsum(int n)
 {
-      int n = binNum;
-      int dec = 0;
-      int pow = 1;
-
+      int sum = 0;
       while (n > 0)
       {
+
             int lastdigit = n % 10;
-            dec += lastdigit * pow;
-            pow = pow * 2;
+            sum += lastdigit;
             n = n / 10;
       }
 
-      cout << dec << endl;
-}
-
-void decTobin(int decnum)
-{
-      int n = decnum;
-      long long  pow = 1;
-      long long  binNum = 0;
-
-      while (n > 0)
-      {
-            int rem = n % 2;
-            binNum += rem * pow;
-            n = n / 2;
-            pow = pow * 10;
-      }
-
-      cout << binNum << endl;
+      return sum;
 }
 
 int main()
 {
 
-      binTOdec(100011100);
-      decTobin(20000084);
+      cout << findsum(436676764);
+
       return 0;
 }
