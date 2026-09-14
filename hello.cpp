c0248be367e339c44d743b682d57e6cf1a2b28c7
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-14 18:47:21 +0530

    Refactor hello.cpp to remove findsum function and update main for pointer demonstration

--- hello.cpp
+++ hello.cpp
@@ -2,24 +2,17 @@
 #include <cmath>
 using namespace std;
 
-int findsum(int n)
-{
-      int sum = 0;
-      while (n > 0)
-      {
 
-            int lastdigit = n % 10;
-            sum += lastdigit;
-            n = n / 10;
-      }
-
-      return sum;
-}
 
 int main()
 {
 
-      cout << findsum(436676764);
+      int a = 1;
+      int *ptr  = &a;
+
+      int **pptr = &ptr;
+      cout<< pptr<<endl;
+
 
       return 0;
 }
