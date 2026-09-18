7228f81cbaeb643f213d4a462bd55a0cb25fbc15
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-18 08:33:58 +0530

    Refactor hello.cpp to replace marks array with arr and update logic for finding max and min values

--- hello.cpp
+++ hello.cpp
@@ -4,15 +4,25 @@ using namespace std;
 
 int main()
 {
+      int arr[6] = {5, 4, 7, 8, 2, 10};
+      int n = sizeof(arr) / sizeof(int);
 
-      int marks[5] = {1, 2, 3, 4, 5};
-      int lenth = sizeof(marks) / sizeof(int);
-
-      for (int i = 0; i <= lenth - 1; i++)
+      int max = arr[0];
+      int min = arr[0];
+      for (int i = 0; i < n; i++)
       {
-            cout<<marks[i]<<" ";
+            if (arr[i] > max)
+            {
+                  max = arr[i];
+                  cout << "assigning val " << arr[i] << " to max" << endl;
+            }
+            if (arr[i] < min)
+            {
+                  min = arr[i];
+            }
       }
-      cout<<endl;
+      cout << "largest in array = " << max << endl;
+      cout << "smallest in array = " << max << endl;
 
       return 0;
 }
