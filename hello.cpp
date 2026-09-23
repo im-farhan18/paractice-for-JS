2c840ed54bb3be75aad2da6cf2c2d46cfa1f4b33
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-23 21:19:24 +0530

    Refactor hello.cpp to remove unused array logic and simplify input handling

--- hello.cpp
+++ hello.cpp
@@ -1,28 +1,14 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main()
 {
-      int arr[6] = {5, 4, 7, 8, 2, 10};
-      int n = sizeof(arr) / sizeof(int);
+    int n;
 
-      int max = arr[0];
-      int min = arr[0];
-      for (int i = 0; i < n; i++)
-      {
-            if (arr[i] > max)
-            {
-                  max = arr[i];
-                  cout << "assigning val " << arr[i] << " to max" << endl;
-            }
-            if (arr[i] < min)
-            {
-                  min = arr[i];
-            }
-      }
-      cout << "largest in array = " << max << endl;
-      cout << "smallest in array = " << min << endl;
+    cout << "Enter a number: ";
+    cin >> n;
 
-      return 0;
+cout<<&n <<endl;
+
+    return 0;
 }
