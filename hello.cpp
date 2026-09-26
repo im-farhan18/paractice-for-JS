59db49b3a50e6e19073607a7a22ed526a6219d19
Farhan Khan <khanfarhan2358@gmail.com>
2026-09-26 08:19:46 +0530

    Refactor hello.cpp to update change function to use reference instead of pointer

--- hello.cpp
+++ hello.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 using namespace std;
 
-void change (int *a ){
-
-    *a += 20 ;
-    cout<<*a <<endl;
+void change (int &a ){
+a =20;
+cout<<a<<endl;
 
 }
 int main()
 {
-    int a = 10;
-    change(&a);
-
+      int a = 10;
+    change(a);
+  
+    // int &b = a;
+    // b = 25;
+    // cout<<b<<endl;
     cout<<a<<endl;
-
+  
 
     return 0;
 }
